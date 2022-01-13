c7cda16bf21d772be89e11053495090dbea9dc57
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-01-13 07:12:36 +0100

    update YAML header with new paths

--- 01_CE-BVerfG_CorpusCreation.R
+++ 01_CE-BVerfG_CorpusCreation.R
@@ -11,9 +11,9 @@
 #'    number_sections: true
 #'    pandoc_args: --listings
 #'    includes:
-#'      in_header: General_Source_TEX_Preamble_DE.tex
-#'      before_body: [CE-BVerfG_Source_TEX_Definitions.tex,CE-BVerfG_Source_TEX_CompilationTitle.tex]
-#'bibliography: packages.bib
+#'      in_header: tex/Preamble_DE.tex
+#'      before_body: [temp/CE-BVerfG_Definitions.tex, tex/CE-BVerfG_CompilationTitle.tex]
+#'bibliography: temp/packages.bib
 #'nocite: '@*'
 #' ---
 
