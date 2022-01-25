1e82c064b7a4f5b2e4600abb172084f4f9499975
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-01-25 11:56:06 +0100

    remvoe comment

--- 01_CE-BVerfG_CorpusCreation.R
+++ 01_CE-BVerfG_CorpusCreation.R
@@ -277,8 +277,7 @@ writeLines(latexdefs,
 
 #'## Parallelisierung aktivieren
 #' Parallelisierung wird zur Beschleunigung der Konvertierung von PDF zu TXT und der Datenanalyse mittels **quanteda** und **data.table** verwendet. Die Anzahl threads wird automatisch auf das verfügbare Maximum des Systems gesetzt, kann aber auch nach Belieben auf das eigene System angepasst werden. Die Parallelisierung kann deaktiviert werden, indem die Variable **fullCores** auf 1 gesetzt wird.
-#'
-#' Die hier verwendete Funktion **makeForkCluster()** ist viel schneller, funktioniert aber nur auf Unix-basierten Systemen (Linux, MacOS). Bei einer Ausführung unter Windows sollten Sie **makecluster()** verwenden.
+
 
 
 #+
