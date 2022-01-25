e0522a88ea6f3fdaab7d0f56176e74b5b3142bcc
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-01-25 11:31:00 +0100

    automatically integrate readme

--- 01_CE-BVerfG_CorpusCreation.R
+++ 01_CE-BVerfG_CorpusCreation.R
@@ -25,60 +25,11 @@ knitr::opts_chunk$set(echo = TRUE,
 
 
 #'\newpage
-#+
-#'# Einleitung
-#'
-#+
-#'## Überblick
-#' Dieses R-Skript lädt alle auf www.bundesverfassungsgericht.de verfügbaren Entscheidungen des Bundesverfassungsgerichts (BVerfG) herunter und verarbeitet sie in einen reichhaltigen menschen- und maschinenlesbaren Korpus. Es ist die Grundlage für den \textbf{\datatitle\ (\datashort )}.
-#'
-#' Alle mit diesem Skript erstellten Datensätze werden dauerhaft kostenlos und urheberrechtsfrei auf Zenodo, dem wissenschaftlichen Archiv des CERN, veröffentlicht. Alle Versionen sind mit einem persistenten Digital Object Identifier (DOI) versehen. Die neueste Version des Datensatzes ist immer über den Link der Concept DOI erreichbar: \dataconcepturldoi
 
 
-
-#+
-#'## Funktionsweise
-
-#' Primäre Endprodukte des Skripts (im Ordner 'output') sind folgende ZIP-Archive:
-#' 
-#' \begin{enumerate}
-#' \item Der volle Datensatz im CSV-Format
-#' \item Die reinen Metadaten im CSV-Format (wie unter 1, nur ohne Entscheidungstexte)
-#' \item (Optional) Tokenisierte Form aller Texte mit linguistischen Annotationen im CSV-Format
-#' \item Alle Entscheidungen im TXT-Format (reduzierter Umfang an Metadaten)
-#' \item Alle Entscheidungen im PDF-Format (reduzierter Umfang an Metadaten)
-#' \item Alle Analyse-Ergebnisse (Tabellen als CSV, Grafiken als PDF und PNG)
-#' \item Der Source Code und alle weiteren Quelldaten
-#' \end{enumerate}
-#'
-#' Zusätzlich werden für alle ZIP-Archive kryptographische Signaturen (SHA2-256 und SHA3-512) berechnet und in einer CSV-Datei hinterlegt. Die Analyse-Ergebnisse werden zum Ende hin nicht gelöscht, damit sie für die Codebook-Erstellung verwendet werden können.  Weiterhin kann optional ein PDF-Bericht erstellt werden (siehe unter "Kompilierung").
-
-
-
-#+
-#'## Kompilierung
-#' Mit der Funktion **render()** von **rmarkdown** können der **vollständige Datensatz** und das **Codebook** kompiliert und die Skripte mitsamt ihrer Rechenergebnisse in ein gut lesbares PDF-Format überführt werden.
-#'
-#' Alle Kommentare sind im roxygen2-Stil gehalten. Die beiden Skripte können daher auch **ohne render()** regulär als R-Skripte ausgeführt werden. Es wird in diesem Fall kein PDF-Bericht erstellt und Diagramme werden nicht abgespeichert.
-#' 
-#' Um den **vollständigen Datensatz** zu kompilieren, sowie Compilation Report und Codebook zu erstellen, kopieren Sie bitte alle im Source-Archiv bereitgestellten Dateien in einen leeren Ordner und führen mit R diesen Befehl aus:
-
-#+ eval = FALSE
-
-source("00_CE-BVerfG_FullCompile.R")
-
-#+
-#'## Systemanforderungen
-#' Das Skript in seiner veröffentlichten Form kann nur unter Linux ausgeführt werden, da es Linux-spezifische Optimierungen (z.B. Fork Cluster) und Shell-Kommandos (z.B. OpenSSL) nutzt. Das Skript wurde unter Fedora Linux entwickelt und getestet. Die zur Kompilierung benutzte Version entnehmen Sie bitte dem **sessionInfo()**-Ausdruck am Ende dieses Berichts.
-#'
-#' In der Standard-Einstellung wird das Skript vollautomatisch die maximale Anzahl an Rechenkernen/Threads auf dem System zu nutzen. Wenn die Anzahl Threads (Variable "fullCores") auf 1 gesetzt wird, ist die Parallelisierung deaktiviert.
-#'
-#' Auf der Festplatte sollten 8 GB Speicherplatz vorhanden sein.
-#' 
-#' Um die PDF-Berichte kompilieren zu können benötigen Sie das R package **rmarkdown**, eine vollständige Installation von \LaTeX\ und alle in der Präambel-TEX-Datei angegebenen \LaTeX\ Packages.
-
-
-
+#+ results = "asis"
+cat(readLines("README.md"),
+    sep = "\n")
 
 
 
