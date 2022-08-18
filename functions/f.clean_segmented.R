0137335eda00f46b5e47ced5e68590b559151dc8
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-08-18 23:23:49 +0200

    add type extraction to segmented clean fun

--- functions/f.clean_segmented.R
+++ functions/f.clean_segmented.R
@@ -16,7 +16,7 @@ f.clean_segmented <- function(x){
 
 
 
-    ## Fehlerhafte ECLI korrigieren
+    ## === ECLI korrigieren ===
 
 
     x$ecli <- gsub("ECLI:DE:BVerfG:1951:rs19580115.1bvr040051",
@@ -38,6 +38,46 @@ f.clean_segmented <- function(x){
     x$ecli <- gsub("ECLI:DE:BVerfG:2015:rs20151208a.1bvr009911",
                    "ECLI:DE:BVerfG:2015:vb20151208.vz000115",
                    x$ecli)
+
+
+
+
+    
+    ## === Entscheidungstyp aus Zitiervorschlägen extrahieren ===
+
+
+
+    ## Zitiervorschläge parsen
+    
+    entscheidung_typ <- gsub(".*(Beschluss|Urteil|Verfügung).*",
+                             "\\1",
+                             x$zitiervorschlag,
+                             ignore.case = TRUE)
+
+
+
+    ## Kürzen
+
+    lang.etyp <- c("Urteil",
+                   "Beschluss",
+                   "Verfügung")
+
+    kurz.etyp <- c("U",
+                   "B",
+                   "V")
+
+
+    entscheidung_typ <- mgsub::mgsub(entscheidung_typ,
+                                          lang.etyp,
+                                          kurz.etyp,
+                                          ignore.case = TRUE)
+
+
+    ## Einfügen
+    x$entscheidung_typ <- entscheidung_typ
+
+
+    
 
 
     return(x)
