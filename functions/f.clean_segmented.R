05a309e529d780229b13b6233aa49d9b984e024a
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-08-19 21:36:33 +0200

    add hyphen remove to segmented clean

--- functions/f.clean_segmented.R
+++ functions/f.clean_segmented.R
@@ -77,6 +77,9 @@ f.clean_segmented <- function(x){
     x$entscheidung_typ <- entscheidung_typ
 
 
+    ## === Über Zeilengrenzen gebrochene Worter zusammenfügen ===
+    
+    x[, text := lapply(.(text), f.hyphen.remove)]
     
 
 
