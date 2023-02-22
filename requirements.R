ac2f40fb82c3eb11bfdf79641be1641600abf5c1
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2023-02-22 13:27:38 +0100

    add R reqs script

---
+++ requirements.R
@@ -0,0 +1,24 @@
+pkgs <- c("data.table",
+          "future",
+          "future.apply",
+          "targets",
+          "tarchetypes",
+          "qs",
+          "RcppTOML",
+          "mgsub",
+          "readtext",
+          "quanteda",
+          "quanteda.textstats",
+          "quanteda.textplots",
+          "spacyr",
+          "udpipe",
+          "igraph",
+          "ggraph",
+          "kableExtra",
+          "pdftools",
+          "renv",
+          "testthat",
+          "zip")
+
+
+install.packages(pkgs)
