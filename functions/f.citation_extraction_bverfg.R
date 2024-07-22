4e3c009d93c173b13128e783b45e3168faa14d1f
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2024-07-22 22:07:31 +0200

    more citation extraction

--- functions/f.citation_extraction_bverfg.R
+++ functions/f.citation_extraction_bverfg.R
@@ -17,19 +17,40 @@ f.citation_extraction_bverfg <- function(dt.final,
     registerzeichen.regex <- paste0(registerzeichen, collapse = "|")
     registerzeichen.regex <- paste0("(", registerzeichen.regex, ")")
 
-    ## Extract outgoing BVerfG citations, example: "2 BvR 454/71"
+
+    ## Create full REGEX, example: "2 BvR 454/71"
     regex <- paste0("[12][[:space:]]*", # Senatsnummer 
                     registerzeichen.regex, # Registerzeichen
                     "[[:space:]]*[0-9]{1,5}/", # Eingangsnummer
                     "[0-9]{2}") # Jahr
     
-    
+    ## Extract BVerfG citations to target Aktenzeichen
     target <- stringi::stri_extract_all(dt.final$text,
                                         regex = regex)
 
+    ## Define source Aktenzeichen
+    source <- dt.final$aktenzeichen
+
+    ## Bind source and target
+    bind <- mapply(cbind, source, target)
+    bind2 <- lapply(bind, as.data.table)
+    
+    dt <- rbindlist(bind2)
+    setnames(dt, new = c("source", "target"))
+    
+    ## Clean whitespace
+    dt$source <- gsub("[[:space:]]+", " ", dt$source)
+    dt$target <- gsub("[[:space:]]+", " ", dt$target)
+
+    dt$source <- trimws(dt$source)
+    dt$target <- trimws(dt$target)
 
 
-    }
+
+    
+    
+
+}
 
 
 
