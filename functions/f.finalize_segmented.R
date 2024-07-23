1f921fa7ddb67e532b1dc422c6022671ed2e8c3e
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2024-07-23 18:58:12 +0200

    add bverfge var, unit tests and amended var order test to segmented final func

--- functions/f.finalize_segmented.R
+++ functions/f.finalize_segmented.R
@@ -58,19 +58,65 @@ f.finalize_segmented <- function(dt.segmented,
              datum)
 
 
-    ## Unit Test: Check variables and set column order
+    ## Add BVerfGE variable
+    dt.final$bverfge  <- ifelse(is.na(dt.final$band),
+                                TRUE,
+                                FALSE)
     
+    ## Unit Test: Check if all variables are documented
     varnames <- gsub("\\\\", "", varnames) # Remove LaTeX escape characters
+    stopifnot(length(setdiff(names(dt.final), varnames)) == 0)
+    varnames <- varnames[!varnames == "segment"] # Remove var for segmented variant
+
+    ## Order variables as in Codebook
     data.table::setcolorder(dt.final, varnames)
 
 
     ## Unit Test
-    test_that("Ergebnis entspricht Erwartungen.", {
+
+    test_that("Klasse ist korrekt.", {
         expect_s3_class(dt.final, "data.table")
+    })
+
+    
+    test_that("Keine Probleme beim Zusammenfügen der Daten.", {
         expect_equal(dt.final[,.N], dt.segmented[,.N])
         expect_lte(uniqueN(dt.final$doc_id), uniqueN(dt.download.final$doc_id))
         expect_gte(dt.final[,.N], dt.bverfg.intermediate[,.N])
     })
+
+    test_that("Datum ist plausibel.", {
+        expect_true(all(dt.final$datum > "1951-01-01"))
+        expect_true(all(dt.final$datum <= Sys.Date()))
+    })
+
+    
+    test_that("Entscheidungsjahr ist plausibel.", {
+        expect_true(all(dt.final$entscheidungsjahr >= 1951))
+        expect_true(all(dt.final$entscheidungsjahr <= year(Sys.Date())))
+    })
+
+    test_that("Spruchkörpertypen sind korrekt.", {
+        expect_setequal(dt.final$spruchkoerper_typ, c("S", "K", "P", "B"))
+    })
+    
+    test_that("Spruchkörpernummern sind korrekt.", {
+        expect_setequal(dt.final$spruchkoerper_az, c(1, 2, NA))
+    })
+    
+    test_that("Registerzeichen sind korrekt.", {
+        expect_setequal(dt.final$registerzeichen, c("BvQ", "BvR", "BvE", "BvL",
+                                                    "BvB", "BvF", "BvH", "BvG",
+                                                    "BvP", "BvN", "BvC", "BvK",
+                                                    "BvM", "PBvU", "Vz"))
+    })
+    
+    test_that("Eingangsnummern sind plausibel.", {
+        expect_true(all(dt.final$eingangsnummer > 0))
+        expect_true(all(dt.final$eingangsnummer < 1e4))
+    })
+
+
 
     
     return(dt.final)
